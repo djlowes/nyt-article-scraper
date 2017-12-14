8a87b2e939151773fccb984d660fd74a02924a35
djlowes <djlowes@gmail.com>
2017-12-13 22:32:35 -0800

    sm

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Articles from "./pages/articles";
+import Articles from "./pages/Article";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
